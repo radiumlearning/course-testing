f7392708a2b0457d97a653cc607c87cc76c576b5
matiasvaz93 <matias.vazquez@radiumrocket.com>
2022-03-08 16:07:30 -0300

    Practice resolution for exercises folder

--- packages/mobile/src/redux/modules/users/actions/exercises/fetchActiveUsers.test.ts
+++ packages/mobile/src/redux/modules/users/actions/exercises/fetchActiveUsers.test.ts
@@ -1,10 +1,67 @@
 import {rest} from 'msw';
 import {server} from 'src/mocks/server';
 import {BASE_API_URL} from '@rn-testing-class/lib/config/mobile';
-import {getMockState, getMockStore} from 'src/test/utils';
+import {getMockStore} from 'src/test/utils';
 import * as Mocks from 'src/mocks';
 
 import {fetchActiveUsersAction} from './fetchActiveUsers';
 
-// TODO Exercise: add tests for this action!
-describe('users/FETCH_ACTIVE_USERS', () => undefined);
+describe('users/FETCH_ACTIVE_USERS', () => {
+  it('fetches active users', async () => {
+    const activeUsersMock = Mocks.users.filter(
+      user => user.status === 'active',
+    );
+
+    const {store, getActionTypes} = getMockStore();
+
+    expect(store.getState().user.activeUsers.data).toEqual([]);
+
+    await expect(
+      store.dispatch(fetchActiveUsersAction()),
+    ).resolves.toMatchObject({
+      payload: Mocks.users.filter(user => user.status === 'active'),
+    });
+
+    expect(getActionTypes()).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "type": "users/FETCH_ACTIVE_USERS/pending",
+        },
+        Object {
+          "type": "users/FETCH_ACTIVE_USERS/fulfilled",
+        },
+      ]
+    `);
+
+    expect(store.getState().user.activeUsers.data).toEqual(activeUsersMock);
+  });
+
+  it('rejects if API fails', async () => {
+    const {store, getActionTypes} = getMockStore();
+
+    server.use(
+      rest.get(`${BASE_API_URL}/users`, (_, res, ctx) => {
+        return res(ctx.status(400));
+      }),
+    );
+
+    await expect(
+      store.dispatch(fetchActiveUsersAction()),
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"Request failed with status code 400"`,
+    );
+
+    expect(getActionTypes()).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "type": "users/FETCH_ACTIVE_USERS/pending",
+        },
+        Object {
+          "type": "users/FETCH_ACTIVE_USERS/rejected",
+        },
+      ]
+    `);
+
+    expect(store.getState().user.activeUsers.data).toEqual([]);
+  });
+});
--- packages/mobile/src/redux/modules/users/actions/exercises/fetchActiveUsers.ts
+++ packages/mobile/src/redux/modules/users/actions/exercises/fetchActiveUsers.ts
@@ -2,22 +2,13 @@ import {createAsyncThunk} from '@reduxjs/toolkit';
 
 import {User} from '@rn-testing-class/lib/interfaces';
 import {AppDispatch, RootState} from '../../../..';
-import {selectHasUsers} from '../../../../selectors/selectHasUsers';
 import * as API from '../../api';
 
 export const fetchActiveUsersAction = createAsyncThunk<
   User[],
   undefined,
   {state: RootState; dispatch: AppDispatch}
->(
-  'users/FETCH_ACTIVE_USERS',
-  async () => {
-    const res = await API.getExample();
-    return res.data.filter(user => user.status === 'active');
-  },
-  {
-    condition: (_, {getState}) => {
-      if (selectHasUsers(getState())) return false;
-    },
-  },
-);
+>('users/FETCH_ACTIVE_USERS', async () => {
+  const res = await API.getUsers();
+  return res.data.filter(user => user.status === 'active');
+});
